e9cd55b34b978bba77043787252c64c19b07a9a1
Jayaleshwari N <jayaleshwarin@syncfusion.com>
2020-04-23 17:41:00 +0530

    XAMARIN-34619 - Sample and screen shot added

---
+++ AccordionXamarin/AccordionXamarin/AccordionXamarin/Model/ItemInfo.cs
@@ -0,0 +1,39 @@
+﻿using System;
+using System.Collections.Generic;
+using System.ComponentModel;
+using System.Text;
+
+namespace AccordionXamarin
+{
+    public class ItemInfo : INotifyPropertyChanged
+    {
+        private string _name;
+        private string _description;
+
+        public string Name
+        {
+            get => _name;
+            set
+            {
+                _name = value;
+                OnPropertyChanged("Name");
+            }
+        }
+        public string Description
+        {
+            get => _description;
+            set
+            {
+                _description = value;
+                OnPropertyChanged("Description");
+            }
+        }
+
+        public event PropertyChangedEventHandler PropertyChanged;
+
+        private void OnPropertyChanged(string Property)
+        {
+            PropertyChanged?.Invoke(this, new PropertyChangedEventArgs(Property));
+        }
+    }
+}
